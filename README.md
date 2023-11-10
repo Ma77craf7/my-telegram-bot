2a05c76b7cb09177a8a5446a5ae4034342d9fc54
Mattcraft <97537869+Ma77craf7@users.noreply.github.com>
2023-11-10 14:16:31 +0100

    modified the progress message (now it do something), added comments and translate in english, updated the README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# my-telegram-bot
-My telegram bot in python
+# My  Telegram Bot
+
+This is a simple Telegram bot written in Python that provides various functionalities, including viewing notes, sharing interesting content, and downloading YouTube videos. The bot is built using the Telegram Bot API and relies on the `yt_dlp` library for YouTube video downloading.
+
+## Features
+
+1. **Start Command**
+   - `/start`: Initiates a conversation with the bot and provides a welcome message.
+
+2. **Appunti Command**
+   - `/appunti`: Shares notes or links to resources. The content is loaded from a configuration file.
+
+3. **Meraviglie Command**
+   - `/meraviglie`: Shares an image with the user. The image path is specified in the configuration file.
+
+4. **YouTube Downloader**
+   - `/yt <video_url>`: Downloads YouTube videos. The downloaded video is sent to the user, and progress messages are provided during the download. **Note: Requires ffmpeg to be installed on your system.**
+
+## Prerequisites
+
+Before running the bot, make sure to create a `config.ini` file in the same directory as the script, containing the following information:
+
+```ini
+[Telegram]
+token = YOUR_TELEGRAM_BOT_TOKEN
+image = PATH_TO_IMAGE_FILE
+site = LINK_TO_SITE_OR_NOTES
+
--- main.py
+++ main.py
@@ -6,31 +6,39 @@ from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler, CallbackContext
 import yt_dlp
 from asyncio import ensure_future
-# Carica il token dal file di configurazione
+
+# Load the token from the configuration file
 config = ConfigParser()
-config.read('config.ini')  # Assicurati che il tuo file di configurazione si trovi nella stessa directory dello script
+config.read('config.ini')  # Make sure your configuration file is in the same directory as the script
 telegram_token = config.get('Telegram', 'token')
 image = config.get('Telegram', 'image')
-site = config.get("Telegram", 'site')
+site = config.get('Telegram', 'site')
+botName = config.get('Telegram', 'name')
 
+# Set up logging configuration
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
- )
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE,site):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hey, sono Barbero, per vedere meravigle usa il commando /ceno")
+)
 
+# Handler for the /start command
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hey, I'm {name}, to see wonders use the /wonders command.")
+
+# Handler for the /appunti command
 async def appunti(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=site)
 
+# Handler for the /meraviglie command
 async def meraviglie(update: Update, context: ContextTypes.DEFAULT_TYPE, image=image):
     chat_id = update.message.chat_id
     await context.bot.send_photo(chat_id, photo=open(image, "rb"))
 
+# Handler for the /yt command to download YouTube videos
 async def download_video(update: Update, context: CallbackContext) -> None:
     video_url = context.args[0] if context.args else None
     if not video_url:
-        await update.message.reply_text('Per favore, fornisci un URL valido di un video di YouTube.')
+        await update.message.reply_text('Please provide a valid YouTube video URL.')
         return
 
     try:
@@ -41,7 +49,7 @@ async def download_video(update: Update, context: CallbackContext) -> None:
         ydl_opts = {
             'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',
             'outtmpl': output_filename,
-            'progress_hooks': [lambda d: ensure_future(download_progress_hook(d, update, context))],
+            'progress_hooks': [lambda d: (ensure_future(download_progress_hook(d, update, context)))],
             'merge_output_format': 'mp4',
         }
 
@@ -49,43 +57,45 @@ async def download_video(update: Update, context: CallbackContext) -> None:
             ydl.download([video_url])
 
         with open(output_filename, 'rb') as video_file:
-            await context.bot.send_video(chat_id, video_file, caption='Video scaricato con successo!')
+            await context.bot.send_video(chat_id, video_file, caption='Video downloaded successfully!')
 
         os.remove(output_filename)
 
     except yt_dlp.DownloadError as e:
-        await update.message.reply_text(f'Errore durante il download da YouTube: {str(e)}')
+        await update.message.reply_text(f'Error downloading from YouTube: {str(e)}')
     except Exception as e:
-        await update.message.reply_text(f'Si è verificato un errore: {str(e)}')
+        await update.message.reply_text(f'An error occurred: {str(e)}')
 
+# Progress hook for video download
 async def download_progress_hook(d: dict, update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
 
-    if d['status'] == 'downloading':
-        message = f'Scaricamento... {d["_percent_str"]} ETA: {d["_eta_str"]}'
-        await context.bot.send_message(chat_id, message)
-    elif d['status'] == 'finished':
-        await context.bot.send_message(chat_id, 'Download completato!')
+    if d['status'] == 'finished':
+        await context.bot.send_message(chat_id, f'Download completed for a file!')
+
     elif d['status'] == 'error':
-        await context.bot.send_message(chat_id, f'Errore durante il download: {d["error"]}')
+        await context.bot.send_message(chat_id, f'Error during download: {d["error"]}')
 
 if __name__ == '__main__':
+    # Build the application
     application = ApplicationBuilder().token(telegram_token).build()
 
-    # start handler
+    # Add handlers for commands
+    # Start command handler
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
 
-    # appunti link
+    # Appunti command handler
     appunti_handler = CommandHandler('appunti', appunti)
     application.add_handler(appunti_handler)
 
-    # meraviglie
-    meravigle_handler = CommandHandler("meraviglie",meraviglie)
+    # Meraviglie command handler
+    meravigle_handler = CommandHandler("meraviglie", meraviglie)
     application.add_handler(meravigle_handler)
 
-    # youtube downloader
+    # YouTube downloader command handler
     yt_handler = CommandHandler('yt', download_video)
     application.add_handler(yt_handler)
 
+    # Run the application with polling
     application.run_polling()
